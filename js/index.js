cc074ce59cad03d5bd6fe8e6c18931b5cfefb837
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-29 00:27:04 +0530

    mouse functions

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 
 console.log("Welcome to JQuery!");
+
+ // $('selector').action();
+ $('h1').click(function(){
+    console.log('Clicked',this);
+    $('h1').hide();
+});
+
+$('h2').click(function(){
+    console.log("Register here");
+    $('.reg').show();
+})
+
